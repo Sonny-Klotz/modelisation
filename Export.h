9af0d392ba9b687e345bca2f77ad84df69721381
root <root@rikitou-PC.home>
2019-03-19 19:41:36 +0100

    Ajout fonction affichage du graphe Validateur : root <root@rikitou-PC.home> Modifications qui seront validées : 	modifié :         Export.c 	modifié :         Export.h

--- Export.h
+++ Export.h
@@ -2,7 +2,8 @@
 #define EXPORT_H
 
 /* Distribution des degrés */
-void gnu(int tab_degres[],int degre_graph,int nombre_sommets,char *nom_fichier);
+void gnu(int tab_degres[],int degre_graph,char *nom_fichier);
 
 /* Affichage du graphe */
+void dessine_graph(Graphe *g,char *nom_fichier);
 #endif
